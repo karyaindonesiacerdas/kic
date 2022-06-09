0d28f8d6ade90fdf4465f5975ccebae8fe958add
Dede Kurniawan <dedekurniawan.dk67@gmail.com>
2022-06-09 09:46:54 +0700

    update loker

--- lowongan-kerja.php
+++ lowongan-kerja.php
@@ -5,90 +5,88 @@
 <body onload="myFunction()">
     <div id="loading"></div>
     <?php include('./layouts/header.php') ?>
-
     <section id="about">
-
         <div class="container wow fadeInUp">
-
             <div class="row">
-
                 <div class="col-md-12">
-
-                    <h3 class="section-title">Lowongan Kerja</h3>
-
+                    <h3 class="section-title text-danger">Lowongan Kerja</h3>
                     <div class="section-title-divider"></div>
-
                     <!-- <p class="section-description">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p> -->
-
                 </div>
-
             </div>
-
         </div>
-
-        <div class="container about-container wow fadeInUp">
-
+        <div class="about-container wow fadeInUp">
             <div class="row">
-
-
-
-                <div class="col-md-2 about-content"></div>
-
-                <div class="col-md-8 about-content">
+                <div class="about-content bg-danger">
                     <!--  <h2 class="about-title">We provide great services and ideass</h2> -->
-                    <p style="text-align: justify;margin-bottom: 20px">
-                        Dibutuhkan segera lulusan d3 atau s1 jurusan manajemen bisnis/ akuntansi / marketing untuk bekerja sebagai pegawai bidang manajemen keuangan dan pemasaran di pt karya indonesia cerdas (<a target="_blank" href="https://karya-indonesia-cerdas.com/">www.karya-indonesia-cerdas.com</a>). </p>
-
-
-                    <p style="text-align: justify; font-size: 20px"><b>Persyaratan</b></p>
-                    <ul style="margin-left: 20px;margin-bottom: 20px; text-align: left;">
-                        <li>Lulusan D3/S1 dalam bidang manajemen bisnis/akuntansi/marketing</li>
-                        <li>Ipk sekurang-kurangnya 3.0</li>
-                        <li>Sanggup bekerja online sekurang-kurangnya 8 jam sehari.</li>
-                        <li>Proaktif dan Komunikatif</li>
-                        <li>Fresh graduate atau memiliki pengalaman dalam bidang yang sama</li>
-                        <li>Bisa berkomunikasi dalam bahasa inggris (minimal pasif)</li>
-                        <li>Dapat menggunakan software akuntansi dan microsoft office</li>
-                        <li>Teliti dan detail</li>
-                        <li>Inovatif dan berpikiran kritis</li>
-                        <li>Mau belajar terus menerus</li>
-                        <li>Dapat bekerja dalam tim dengan kecepatan tinggi</li>
-                        <li>Tahan terhadap tekanan dari dalam maupun luar perusahaan</li>
-                        <li>Sanggup bekerja baik di rumah (WFH) atau bekerja di office</li>
-                        <li>Mampu membuat rencana mingguan dan harian, serta sanggup melaporkan rencana dan progress kerja harian</li>
-                        <li>Memiliki komitmen tinggi dan pantang menyerah dalam menyelesaikan</li>
-                        <li>Pekerjaan sesuai rencana</li>
-
-                    </ul>
-
-                    <p style="text-align: justify;font-size: 20px"><b>Benefit</b></p>
-                    <ul style="margin-left: 20px;margin-bottom: 20px; text-align: left;">
-                        <li>Gaji pokok berdasarkan UMR domisili</li>
-                        <li>Bonus berdasarkan pencapaian</li>
-                        <li>Dapat bekerja dari rumah</li>
-                        <li>Tunjangan Kesehatan</li>
-                    </ul>
-
-
-                    <p style="text-align: justify;font-size: 20px"><b>Hubungi</b></p>
-                    <p style="text-align: justify;">Jika anda tertarik untuk bekerja di PT Karya Indonesia Cerdas dan memenuhi persyaratan diatas, segera email CV anda ke <b>info@karya-indonesia-cerdas.com</b> atau WA <b>081573309155</b></p>
-
-
-
+                    <!-- <div>
+                        <p style="text-align: justify;margin-bottom: 20px">
+                            Dibutuhkan segera lulusan d3 atau s1 jurusan manajemen bisnis/ akuntansi / marketing untuk bekerja sebagai pegawai bidang manajemen keuangan dan pemasaran di pt karya indonesia cerdas (<a target="_blank" href="https://karya-indonesia-cerdas.com/">www.karya-indonesia-cerdas.com</a>). </p>
+    
+    
+                        <p style="text-align: justify; font-size: 20px"><b>Persyaratan</b></p>
+                        <ul style="margin-left: 20px;margin-bottom: 20px; text-align: left;">
+                            <li>Lulusan D3/S1 dalam bidang manajemen bisnis/akuntansi/marketing</li>
+                            <li>Ipk sekurang-kurangnya 3.0</li>
+                            <li>Sanggup bekerja online sekurang-kurangnya 8 jam sehari.</li>
+                            <li>Proaktif dan Komunikatif</li>
+                            <li>Fresh graduate atau memiliki pengalaman dalam bidang yang sama</li>
+                            <li>Bisa berkomunikasi dalam bahasa inggris (minimal pasif)</li>
+                            <li>Dapat menggunakan software akuntansi dan microsoft office</li>
+                            <li>Teliti dan detail</li>
+                            <li>Inovatif dan berpikiran kritis</li>
+                            <li>Mau belajar terus menerus</li>
+                            <li>Dapat bekerja dalam tim dengan kecepatan tinggi</li>
+                            <li>Tahan terhadap tekanan dari dalam maupun luar perusahaan</li>
+                            <li>Sanggup bekerja baik di rumah (WFH) atau bekerja di office</li>
+                            <li>Mampu membuat rencana mingguan dan harian, serta sanggup melaporkan rencana dan progress kerja harian</li>
+                            <li>Memiliki komitmen tinggi dan pantang menyerah dalam menyelesaikan</li>
+                            <li>Pekerjaan sesuai rencana</li>
+    
+                        </ul>
+    
+                        <p style="text-align: justify;font-size: 20px"><b>Benefit</b></p>
+                        <ul style="margin-left: 20px;margin-bottom: 20px; text-align: left;">
+                            <li>Gaji pokok berdasarkan UMR domisili</li>
+                            <li>Bonus berdasarkan pencapaian</li>
+                            <li>Dapat bekerja dari rumah</li>
+                            <li>Tunjangan Kesehatan</li>
+                        </ul>
+    
+    
+                        <p style="text-align: justify;font-size: 20px"><b>Hubungi</b></p>
+                        <p style="text-align: justify;">Jika anda tertarik untuk bekerja di PT Karya Indonesia Cerdas dan memenuhi persyaratan diatas, segera email CV anda ke <b>info@karya-indonesia-cerdas.com</b> atau WA <b>081573309155</b></p>
+                    </div> -->
+                    <div class="row">
+                        <div class="col-sm-offset-3 col-sm-6">
+                            <div class="panel panel-default">
+                                <ul class="list-group">
+                                    <li class="list-group-item">
+                                        <a href="assets/loker/LOWONGAN KERJA _Project Manager_FULL STACK PROGRAMMER.pdf" target="_blank">
+                                            Project Manager (Link Persyaratan)
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="assets/loker/LOWONGAN KERJA_BACK END.pdf" target="_blank">
+                                            Back End (Link Persyaratan)
+                                        </a>
+                                    </li>
+                                    <li class="list-group-item">
+                                        <a href="assets/loker/LOWONGAN KERJA_FRONT END.pdf" target="_blank">
+                                            Front End (Link Persyaratan)
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
                 <div class="col-md-2 about-content"></div>
             </div>
-
         </div>
-
     </section>
-
-
     <?php include("./layouts/footer.php") ?>
     <?php include("./body.php") ?>
-
-
 </body>
 
 </html>
