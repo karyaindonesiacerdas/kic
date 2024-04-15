26ad237cae2b28bde58901630a423a38db467978
Dede Kurniawan <dedekurniawan.dk67@gmail.com>
2024-04-15 09:09:44 +0700

    update banner inahef

--- home/hero.php
+++ home/hero.php
@@ -5,16 +5,16 @@
              <a class="next" href="#">&#xf054;</a>
          </div>
          <div class="slides">
-             <div class="slide active blue">
-             </div>
+             <div class="slide active blue"></div>
              <div class="slide white">
                  <img animated class="anim fadeInDown" style="width: 300px" src="../assets/img/single_page_logo.png" alt="">
                  <h2 style="text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;"><b>PT Karya Indonesia Cerdas</b></span></h2>
                  <p style="text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;">"Menjadikan Indonesia yang lebih cerdas adalah bisnis kami"</p>
              </div>
-             <div class="slide newGate">
-                 <a class="btn btn-primary btn-lg" style="position: absolute; bottom: 0;" href="https://drive.google.com/file/d/1dR9P_EeUHfDW1Q7kCybnssa3EX2m04X7/view?usp=sharing" target="_blank" role="button">Download PDF</a>
-             </div>
+             <div class="slide slide-three"></div>
+             <!-- <div class="slide newGate">
+                 <a class="btn btn-primary btn-lg" style="position: absolute; bottom: 0;" href="../assets/img/inahef.jpg" target="_blank" role="button">Download PDF</a>
+             </div> -->
          </div>
          <div class="bullets">
          </div>
